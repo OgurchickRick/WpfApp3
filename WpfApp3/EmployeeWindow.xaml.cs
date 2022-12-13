475e8eeb0b9608e333fbbb0b6f1a6d23a39e47e0
OgurchickRick <artem.derbin.04@list.ru>
2022-12-14 00:36:52 +0300

    Типо валидация

--- WpfApp3/EmployeeWindow.xaml.cs
+++ WpfApp3/EmployeeWindow.xaml.cs
@@ -1,20 +1,32 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfApp3
 {
     public partial class EmployeeWindow : Window
     {
         public Employee Employee { get; private set; }
-        public EmployeeWindow(Employee user)
+        public EmployeeWindow(Employee employee)
         {
             InitializeComponent();
-            Employee = user;
+            Employee = employee;
             DataContext = Employee;
         }
 
         void Accept_Click(object sender, RoutedEventArgs e)
         {
-            DialogResult = true;
+            //Типо валидация, очень страшная, но работает
+            //Костыль на костыле, пока просто чтобы работало
+            bool isValid = fieldSurname.Text.Length > 0
+                & fieldName.Text.Length > 0
+                & fieldPatronymic.Text.Length > 0
+                & fieldBirth.Text.Length > 0
+                & fieldPhone.Text.Length > 0
+                & fieldDepartment.Text.Length > 0;
+            if (isValid) 
+            {
+                DialogResult = true;
+            }
         }
     }
 }
